531adc2cf80a2f117df94c3c3876fc07cb6b6d06
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-08-07 23:15:34 +0500

    completed exercise 4.2

--- shautil.go
+++ shautil.go
@@ -12,27 +12,36 @@ import (
 	"fmt"
 )
 
-var s256 = flag.Bool("sha256", false, "usage: -sha256 <symbol>, example: -sha256 1")
-var s384 = flag.Bool("sha384", false, "usage: -sha384 <symbol>, example: -sha384 2")
-var s512 = flag.Bool("sha512", false, "usage: -sha512 <symbol>, example: -sha512 3")
+var s256 = flag.Bool("sha256", false, "Флаг активирует вывод дайджеста в формате sha256")
+var s384 = flag.Bool("sha384", false, "Флаг активирует вывод дайджеста в формате sha384")
+var s512 = flag.Bool("sha512", false, "Флаг активирует вывод дайджеста в формате sha512")
+var verb = flag.Bool("v", false, "При активации флага активируется подробный отладочный вывод")
 
 func Checksum(s string) string {
 	flag.Parse()
 
 	if !*s256 && !*s384 && !*s512 { // по умолчанию конвертируем в контрольную сумму sha256
-		fmt.Printf("Входящие данные: %s конвертируем в контрольную сумму(sha256 checksum):\n%x\n\n", s, sha256.Sum256([]byte(s)))
+		if *verb {
+			fmt.Printf("Входящие данные: %s конвертируем в контрольную сумму(sha256 checksum):\n%x\n\n", s, sha256.Sum256([]byte(s)))
+		}
 		b := sha256.Sum256([]byte(s))
 		return fmt.Sprintf("%x", b)
 	} else if *s256 {
-		fmt.Printf("Входящие данные: %s, конвертируем в контрольную сумму(sha256 checksum):\n%x\n\n", s, sha256.Sum256([]byte(s)))
+		if *verb {
+			fmt.Printf("Входящие данные: %s, конвертируем в контрольную сумму(sha256 checksum):\n%x\n\n", s, sha256.Sum256([]byte(s)))
+		}
 		b := sha256.Sum256([]byte(s))
 		return fmt.Sprintf("%x", b)
 	} else if *s384 {
-		fmt.Printf("Входящие данные: %s, конвертируем в контрольную сумму(sha384 checksum):\n%x\n\n", s, sha512.Sum384([]byte(s)))
+		if *verb {
+			fmt.Printf("Входящие данные: %s, конвертируем в контрольную сумму(sha384 checksum):\n%x\n\n", s, sha512.Sum384([]byte(s)))
+		}
 		b := sha512.Sum384([]byte(s))
 		return fmt.Sprintf("%x", b)
 	} else if *s512 {
-		fmt.Printf("Входящие данные: %s конвертируем в контрольную сумму(sha512 checksum):\n%x\n\n", s, sha512.Sum512([]byte(s)))
+		if *verb {
+			fmt.Printf("Входящие данные: %s конвертируем в контрольную сумму(sha512 checksum):\n%x\n\n", s, sha512.Sum512([]byte(s)))
+		}
 		b := sha512.Sum512([]byte(s))
 		return fmt.Sprintf("%x", b)
 	}
